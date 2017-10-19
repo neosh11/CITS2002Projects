e18099cf87588f76f665034584934799d6a7b5c3
neosh <neosh11@gmail.com>
2017-10-19 18:12:28 +0800

    fix

--- proj2/mystuff.h
+++ proj2/mystuff.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #define foreach(i,x,y) for(int i = x; i < y; i++)
-#define printfln(S, ...) printf(S, ##__VA_ARGS__); printf("\n");
 #define eval(X) X
 
 extern void lala(int x);
