3cfe2d1307af8353e8665adf975b6e3ec2240071
neosh <neosh11@gmail.com>
2017-10-19 23:28:43 +0800

    fix

--- proj2/myshell.c
+++ proj2/myshell.c
@@ -10,67 +10,71 @@
 
 int main(int argc, char *argv[])
 {
-//  REMEMBER THE PROGRAM'S NAME (TO REPORT ANY LATER ERROR MESSAGES)
-    argv0	= (argv0 = strrchr(argv[0],'/')) ? argv0+1 : argv[0];
-    argc--;				// skip 1st command-line argument
+    //  REMEMBER THE PROGRAM'S NAME (TO REPORT ANY LATER ERROR MESSAGES)
+    argv0 = (argv0 = strrchr(argv[0], '/')) ? argv0 + 1 : argv[0];
+    argc--; // skip 1st command-line argument
     argv++;
+
+    //  INITIALIZE THE THREE INTERNAL VARIABLES
+    HOME = getenv("HOME");
+    if (HOME == NULL)
+    {
+        HOME = DEFAULT_HOME;
+    }
+
+    PATH = getenv("PATH");
+    if (PATH == NULL)
+    {
+        PATH = DEFAULT_PATH;
+    }
+
+    CDPATH = getenv("CDPATH");
+    if (CDPATH == NULL)
+    {
+        CDPATH = DEFAULT_CDPATH;
+    }
 
     check(1);
 
     //STORE ALL PATH VARIABLES IN A LIST
     initializeList();
-    
+
     check(2);
-    char * valuePath = strtok(PATH, ":");
+    char *valuePath = strtok(PATH, ":");
     check(3);
     printS(valuePath);
     enqueue(valuePath);
     check(4);
-    while( (valuePath = strtok(NULL, ":")) != NULL )
+    while ((valuePath = strtok(NULL, ":")) != NULL)
     {
         enqueue(valuePath);
     }
     display();
 
+    //  DETERMINE IF THIS SHELL IS INTERACTIVE
+    interactive = (isatty(fileno(stdin)) && isatty(fileno(stdout)));
 
-//  INITIALIZE THE THREE INTERNAL VARIABLES
-    HOME	= getenv("HOME");
-    if(HOME == NULL) {
-	HOME	= DEFAULT_HOME;
+    int exitstatus = EXIT_SUCCESS;
+
+    //  READ AND EXECUTE COMMANDS FROM stdin UNTIL IT IS CLOSED (with control-D)
+    while (!feof(stdin))
+    {
+        SHELLCMD *t = parse_shellcmd(stdin);
+
+        if (t != NULL)
+        {
+
+            //  WE COULD DISPLAY THE PARSED COMMAND-TREE, HERE, BY CALLING:
+            //print_shellcmd(t);
+
+            exitstatus = execute_shellcmd(t);
+
+            free_shellcmd(t);
+        }
     }
-
-    PATH	= getenv("PATH");
-    if(PATH == NULL) {
-	PATH	= DEFAULT_PATH;
-    }
-
-    CDPATH	= getenv("CDPATH");
-    if(CDPATH == NULL) {
-	CDPATH	= DEFAULT_CDPATH;
-    }
-
-//  DETERMINE IF THIS SHELL IS INTERACTIVE
-    interactive		= (isatty(fileno(stdin)) && isatty(fileno(stdout)));
-
-    int exitstatus	= EXIT_SUCCESS;
-
-//  READ AND EXECUTE COMMANDS FROM stdin UNTIL IT IS CLOSED (with control-D)
-    while(!feof(stdin)) {
-	SHELLCMD	*t = parse_shellcmd(stdin);
-
-	if(t != NULL) {
-
-//  WE COULD DISPLAY THE PARSED COMMAND-TREE, HERE, BY CALLING:
-        //print_shellcmd(t);
-
-        exitstatus = execute_shellcmd(t);
-
-        free_shellcmd(t);
-        
-	}
-    }
-    if(interactive) {
-	fputc('\n', stdout);
+    if (interactive)
+    {
+        fputc('\n', stdout);
     }
     return exitstatus;
 }
