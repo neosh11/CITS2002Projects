0cf9576d698032d0a7303b0f4dd2b643c141541d
neosh <neosh11@gmail.com>
2017-10-19 20:36:29 +0800

    fix

--- proj2/execute.c
+++ proj2/execute.c
@@ -24,55 +24,59 @@ int execute_shellcmd(SHELLCMD *t)
     }
     else
     { // normal, exit commands
-
-        int status;
-
-        switch (fork())
-        {
-
-        case -1:
-        {
-
-            exitstatus = EXIT_FAILURE;
-            break;
-        }
-
-        case 0:
-
-        {
-            char *location1 = locationCommand("/bin/", t->argv[0]);
-            char *location2 = locationCommand("/usr/bin/", t->argv[0]);
-
-            struct stat stat_buffer1;
-            struct stat stat_buffer2;
-
-            if (stat(location1, &stat_buffer1) != 0)
-            {
-                if (stat(location2, &stat_buffer2) != 0)
-                {
-                    //doesn't exist
-                }
-                else if (S_ISREG(stat_buffer2.st_mode))
-                {
-                    execv(location2, argumentsArray(t->argc, t->argv));
-                }
-            }
-            else if (S_ISREG(stat_buffer1.st_mode))
-            {
-                execv(location1, argumentsArray(t->argc, t->argv));
-            }
-
-            exitstatus = EXIT_SUCCESS;
-            break;
-        }
-        default:
-        {
-            wait(&status);
-            exitstatus = status;
-            break;
-        }
-        }
+        exitstatus = basicCommands(t);
+        
     }
 
     return exitstatus;
+}
+
+int basicCommands(SHELLCMD *t)
+{
+    int status;
+    switch (fork())
+    {
+
+    case -1:
+    {
+
+        status = EXIT_FAILURE;
+        break;
+    }
+
+    case 0:
+
+    {
+        char *location1 = locationCommand("/bin/", t->argv[0]);
+        char *location2 = locationCommand("/usr/bin/", t->argv[0]);
+
+        struct stat stat_buffer1;
+        struct stat stat_buffer2;
+
+        if (stat(location1, &stat_buffer1) != 0)
+        {
+            if (stat(location2, &stat_buffer2) != 0)
+            {
+                status = EXIT_FAILURE;
+            }
+            else if (S_ISREG(stat_buffer2.st_mode))
+            {
+                execv(location2, argumentsArray(t->argc, t->argv));
+            }
+        }
+        else if (S_ISREG(stat_buffer1.st_mode))
+        {
+            execv(location1, argumentsArray(t->argc, t->argv));
+        }
+
+        status = EXIT_SUCCESS;
+        break;
+    }
+    default:
+    {
+        wait(&status);
+        break;
+    }
+    }
+    return status;
 }
