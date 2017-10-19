5cee0824c7a49a80f5b21ede0c3b053c12aeeb08
neosh <neosh11@gmail.com>
2017-10-19 21:32:08 +0800

    fix

--- proj2/mystuff.c
+++ proj2/mystuff.c
@@ -5,8 +5,8 @@ char **argumentsArray(int size,char **array)
 {
 	char **args = calloc(size, size*(sizeof(char*))+1);
 	
-	foreach(i, 1, size)
-		args[i-1] = &array[i][0];
+	foreach(i, 0, size)
+		args[i] = &array[i][0];
 		
 	return args;
 }
