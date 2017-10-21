685f1c43b50da2d7931f3cd9b08ea21c938afa94
neosh <neosh11@gmail.com>
2017-10-21 21:59:05 +0800

    Just a test function to test piping

---
+++ proj2/piping.c
@@ -0,0 +1,37 @@
+#include  <stdio.h>
+#include  <sys/types.h>
+#include  <sys/stat.h>
+#include  <sys/param.h>
+#include  <dirent.h>
+#include  <unistd.h>
+#include <sys/wait.h>
+#include <sys/time.h>
+#include <fcntl.h>
+
+int main(int argc, char * argv[])
+{
+    int pipe1[2];
+
+    if((pipe(pipe1)) != 0)
+    {
+        printf("FAIL");
+    }
+
+    switch(fork())
+    {
+    case -1:
+
+        printf("FAIL" pipe);
+    case 0:
+        close(pipe1[0]);
+        dup2(pipe[1], 1);
+
+        execv("/bin/ls", (char* []) {ls, NULL};
+    default:
+        wait(NULL);
+        close(pipe1[1]);
+        dup2(pipe[0], 0);
+        execv("/usr/bin/sort", (char* []) {sort, NULL};
+    }
+    return 0;
+}
