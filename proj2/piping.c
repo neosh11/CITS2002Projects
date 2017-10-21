e4418df9b65c6658ed462481ff91d0b62a513e3b
neosh <neosh11@gmail.com>
2017-10-21 22:01:59 +0800

    Just a test function to test piping

--- proj2/piping.c
+++ proj2/piping.c
@@ -21,17 +21,17 @@ int main(int argc, char * argv[])
     {
     case -1:
 
-        printf("FAIL" pipe);
+        printf("FAIL");
     case 0:
         close(pipe1[0]);
         dup2(pipe[1], 1);
 
-        execv("/bin/ls", (char* []) {ls, NULL};
+        execv("/bin/ls", (char* []) {"ls", NULL});
     default:
         wait(NULL);
         close(pipe1[1]);
         dup2(pipe[0], 0);
-        execv("/usr/bin/sort", (char* []) {sort, NULL};
+        execv("/usr/bin/sort", (char* []) {"sort", NULL});
     }
     return 0;
 }
